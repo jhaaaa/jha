23d3a5bbd52d3ea8e2b197d4fc6515806ac760da
Jennifer Hasegawa <jennifer.hasegawa@gmail.com>
2014-01-25 21:35:14 -0800

    Updated with c/p content

--- node/tasklist/task.js
+++ node/tasklist/task.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose')
-	, Schema = mongoose.Schema;
-	
+  , Schema = mongoose.Schema;
+  
 var TaskSchema = new Schema({
-	  itemName		: String
-	, itemCategory 	: String
-	, itemCompleted	: { type: Boolean, default: false }
-	, itemDate		: {type: Date, default: Date.now }
+    itemName      : String
+  , itemCategory  : String
+  , itemCompleted : { type: Boolean, default: false }
+  , itemDate      : { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('TaskModel', TaskSchema);
